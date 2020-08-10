476454d7f1f15fa1b58c6777dcb37a723a4e692a
sathya <karpagasathya@gmail.com>
2020-08-10 17:15:56 -0500

    header design

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+function Navbar() {
+    return (
+      <div className="header-container">
+        <div className="row d-flex justify-content-center">
+          <div className="col-8">
+            <h1>
+              <strong>Employee Directory</strong>
+            </h1>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Navbar; 
