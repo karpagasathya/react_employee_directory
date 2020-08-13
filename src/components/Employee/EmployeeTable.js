a22808e39d1e853a9cd7390d72fe5e062c9d38a2
sathya <karpagasathya@gmail.com>
2020-08-13 00:14:21 -0500

    employee table finished

---
+++ src/components/Employee/EmployeeTable.js
@@ -0,0 +1,24 @@
+import React from "react";
+import EmployeeTableHeader from "./EmployeeTableHeader";
+import EmployeeTableRow from "./EmployeeTableRow";
+
+function EmployeeTable(props) {
+  return (
+    <div className="container-fluid">
+      <div className="row d-flex justify-content-center">
+        <div className="col-10">
+          <table className="table table-striped table-responsive-lg">
+            <thead>{<EmployeeTableHeader handleSort={props.handleSort} sortByCategory={props.sortByCategory} />}</thead>
+            <tbody>
+              {props.employees.map((employee) => (
+                <EmployeeTableRow name={employee.name.first + " " + employee.name.last} role={employee.role} phone={employee.phone} email={employee.email} dob={employee.dob} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default EmployeeTable;
